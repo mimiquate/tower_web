b930d8581a8086481895016f4bc6cac66edf5d91
Mariana Gonzalez <mariana@mimiquate.com>
2026-05-18 16:38:46 -0300

    implement a liveview to render an empty space and router

--- lib/tower_web/router.ex
+++ lib/tower_web/router.ex
@@ -2,11 +2,14 @@ defmodule TowerWeb.Router do
   @moduledoc false
 
   defmacro tower_dashboard(path, opts \\ []) do
+    caller_module = __CALLER__.module
+    app_name = extract_app_name(caller_module)
 
-    quote bind_quoted: [path: path, opts: opts] do
+    quote bind_quoted: [path: path, opts: opts, app_name: app_name] do
+      Application.put_env(:tower_web, :app_name, app_name)
 
       scope path, alias: false, as: false do
-        import Phoenix.LiveView.Router, only: [live: 3, live_session: 3]
+        import Phoenix.LiveView.Router, only: [live: 4, live_session: 3]
 
         session_name = Keyword.get(opts, :as, :tower_dashboard)
         on_mount = Keyword.get(opts, :on_mount, [])
@@ -14,9 +17,17 @@ defmodule TowerWeb.Router do
         live_session session_name,
           on_mount: on_mount,
           root_layout: {TowerWeb.Layouts, :root} do
-          live "/", TowerWeb.Live.Occurrences.Index, :index
+          live "/", TowerWeb.Live.Home, :index, as: session_name
         end
       end
     end
+  end
+
+  defp extract_app_name(module) do
+    module
+    |> Module.split()
+    |> List.first()
+    |> String.replace(~r/Web$/, "")
+    |> String.replace(~r/([a-z])([A-Z])/, "\\1 \\2")
   end
 end
