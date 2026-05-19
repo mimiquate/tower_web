dac2708b1aa403488b55a1ce0f2a474a6976d47d
Mariana Gonzalez <mariana@mimiquate.com>
2026-05-19 16:08:31 -0300

    fix missing embed_templates and add LiveView JS configuration

--- lib/tower_web/router.ex
+++ lib/tower_web/router.ex
@@ -9,7 +9,7 @@ defmodule TowerWeb.Router do
       Application.put_env(:tower_web, :app_name, app_name)
 
       scope path, alias: false, as: false do
-        import Phoenix.LiveView.Router, only: [live: 4, live_session: 3]
+        import Phoenix.LiveView.Router, only: [live: 3, live_session: 3]
 
         session_name = Keyword.get(opts, :as, :tower_dashboard)
         on_mount = Keyword.get(opts, :on_mount, [])
@@ -17,7 +17,7 @@ defmodule TowerWeb.Router do
         live_session session_name,
           on_mount: on_mount,
           root_layout: {TowerWeb.Layouts, :root} do
-          live "/", TowerWeb.Live.Home, :index, as: session_name
+          live "/", TowerWeb.Live.Occurrences.Index, :index
         end
       end
     end
