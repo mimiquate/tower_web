a3062f378ffff405e39cbd35cfe1838651861cfc
Mariana Gonzalez <mariana@mimiquate.com>
2026-06-16 17:04:39 -0300

    use host application base path

--- lib/tower_web/live/occurrences/show.ex
+++ lib/tower_web/live/occurrences/show.ex
@@ -4,16 +4,16 @@ defmodule TowerWeb.Live.Occurrences.Show do
   alias TowerDB.Events
 
   @impl Phoenix.LiveView
-  def mount(%{"id" => id}, _session, socket) do
+  def mount(%{"id" => id}, session, socket) do
     event = Events.get_event!(id)
-    {:ok, assign(socket, event: event)}
+    {:ok, assign(socket, [event: event, base_path: session["base_path"]])}
   end
 
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
     <div class="pt-6 px-10 pb-10">
-      <.back_button navigate="/tower" />
+      <.back_button navigate={"#{@base_path}"} />
 
       <div class="text-lg font-mono text-white mb-6">
         #{@event.id}
--- lib/tower_web/router.ex
+++ lib/tower_web/router.ex
@@ -8,6 +8,8 @@ defmodule TowerWeb.Router do
     quote bind_quoted: [path: path, opts: opts, app_name: app_name] do
       Application.put_env(:tower_web, :app_name, app_name)
 
+      scoped_path = Phoenix.Router.scoped_path(__MODULE__, path)
+
       scope path, alias: false, as: false do
         import Phoenix.LiveView.Router, only: [live: 3, live_session: 3]
 
@@ -16,7 +18,8 @@ defmodule TowerWeb.Router do
 
         live_session session_name,
           on_mount: on_mount,
-          root_layout: {TowerWeb.Layouts, :root} do
+          root_layout: {TowerWeb.Layouts, :root},
+          session: %{"base_path" => scoped_path} do
           live "/", TowerWeb.Live.Occurrences.Index, :index
           live "/:id", TowerWeb.Live.Occurrences.Show, :show
         end
