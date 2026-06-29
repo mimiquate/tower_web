ad20109c66756fefaa2f9a07f197e187721ecdcd
Mariana Gonzalez <mariana@mimiquate.com>
2026-06-29 16:28:45 -0300

    update index and show tests

--- test/tower_web/live/occurrences/show_test.exs
+++ test/tower_web/live/occurrences/show_test.exs
@@ -17,11 +17,12 @@ defmodule TowerWeb.Live.Occurrences.ShowTest do
 
       events = Events.list_events(repo: TowerWeb.TestRepo)
       html = render_component(&Index.render/1, %{
-        events: events,
+        filtered_events: events,
+        search_query: "",
+        base_path: "/tower",
         page: 1,
         total_pages: 1,
         total_count: 1,
-        base_path: "/tower",
         flash: %{}
       })
 
