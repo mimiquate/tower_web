ec9fd590998b1e94672901380f0246468e896046
Mariana Gonzalez <mariana@mimiquate.com>
2026-06-29 16:28:02 -0300

    use history.back to back button

--- lib/tower_web/components/core_components.ex
+++ lib/tower_web/components/core_components.ex
@@ -15,15 +15,13 @@ defmodule TowerWeb.CoreComponents do
     """
   end
 
-  attr :navigate, :string, required: true
-
   def back_button(assigns) do
     ~H"""
-    <.link navigate={@navigate} class="inline-flex items-center justify-center size-12 bg-tower-level-bg mb-6">
+    <button onclick="history.back()" class="inline-flex items-center justify-center size-12 bg-tower-level-bg mb-6">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 hover:size-7 text-white">
         <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
       </svg>
-    </.link>
+    </button>
     """
   end
 
