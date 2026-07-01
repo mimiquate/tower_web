fa1814610ff56d37b24dbda239f35bb8dc18b580
Mariana Gonzalez <mariana@mimiquate.com>
2026-07-01 14:19:36 -0300

    remove a class in search input

--- lib/tower_web/live/occurrences/index.ex
+++ lib/tower_web/live/occurrences/index.ex
@@ -63,7 +63,7 @@ defmodule TowerWeb.Live.Occurrences.Index do
 
     <.page_header title="Occurrences" subtitle="Track occurrences" />
 
-    <form phx-change="search" phx-submit="search" class="w-full h-12 px-3 py-2 flex items-center gap-2 border border-tower-line-color rounded mb-4">
+    <form phx-change="search" phx-submit="search" class="w-full h-12 px-3 py-2 flex items-center gap-2 border border-tower-line-color mb-4">
       <svg class="w-6 h-6 text-tower-text-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
       </svg>
