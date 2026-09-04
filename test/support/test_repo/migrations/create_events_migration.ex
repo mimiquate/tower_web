af590ae5fc78705d1ce9b52723e7565f642bdd0b
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2026-09-04 15:01:03 -0300

    fix test were failing because latest migration were not applied
    Regression introduce here: https://github.com/mimiquate/tower_web/commit/e0f885c5bc10314a73e7eeb6f282a392f71a10f9
    Exposed when we update tower_db here https://github.com/mimiquate/tower_web/commit/c98377cca4ce64ba3267be1d7b8adf35b1110863#diff-dfa6f4ed74c90e5d4fda283d547d366586e690387289bcfd473e3fa5f9ace308R30

--- test/support/test_repo/migrations/create_events_migration.ex
+++ test/support/test_repo/migrations/create_events_migration.ex
@@ -1,6 +1,6 @@
 defmodule TowerWeb.TestRepo.Migrations.CreateEvents do
   use Ecto.Migration
 
-  def up, do: TowerDB.Migration.up(from: 0, to: 4)
-  def down, do: TowerDB.Migration.down(from: 4, to: 0)
+  def up, do: TowerDB.Migration.up(from: 0, to: 7)
+  def down, do: TowerDB.Migration.down(from: 7, to: 0)
 end
