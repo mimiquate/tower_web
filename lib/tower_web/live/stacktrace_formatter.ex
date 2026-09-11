b66407f5f12d608d5e95de41ca10e612b6e7ae1d
Mariana Gonzalez <mariana@mimiquate.com>
2026-09-11 13:49:07 -0300

    fix: render the last line of stacktrace related to the app host

--- lib/tower_web/live/stacktrace_formatter.ex
+++ lib/tower_web/live/stacktrace_formatter.ex
@@ -1,14 +1,23 @@
 defmodule TowerWeb.Live.StacktraceFormatter do
-  def last_stacktrace_line(nil), do: nil
-  def last_stacktrace_line([]), do: nil
+  def last_stacktrace_line(stacktrace, host_otp_app \\ nil)
 
-  def last_stacktrace_line(stacktrace) when is_list(stacktrace) do
+  def last_stacktrace_line(nil, _host_otp_app), do: nil
+  def last_stacktrace_line([], _host_otp_app), do: nil
+
+  def last_stacktrace_line(stacktrace, host_otp_app) when is_list(stacktrace) do
     stacktrace
-    |> List.first()
+    |> Enum.find(&host_app_entry?(&1, host_otp_app))
+    |> Kernel.||(List.first(stacktrace))
     |> format_mfa_entry()
   end
 
-  def last_stacktrace_line(_stacktrace), do: nil
+  def last_stacktrace_line(_stacktrace, _host_otp_app), do: nil
+
+  defp host_app_entry?(_entry, nil), do: false
+
+  defp host_app_entry?({module, _function, _arity_or_args, _location}, host_otp_app) do
+    match?({:ok, ^host_otp_app}, :application.get_application(module))
+  end
 
   defp format_mfa_entry(nil), do: nil
 
