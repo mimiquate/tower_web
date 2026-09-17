2f35e4a5151bff793962bc4e8284e57ce3455888
Mariana Gonzalez <mariana@mimiquate.com>
2026-09-17 12:26:53 -0300

    chore: run mix format

--- lib/tower_web/components/core_components.ex
+++ lib/tower_web/components/core_components.ex
@@ -556,7 +556,12 @@ defmodule TowerWeb.CoreComponents do
   attr(:host_otp_app, :atom, required: true)
 
   def last_stacktrace_line(assigns) do
-    assigns = assign(assigns, :line, StacktraceFormatter.last_stacktrace_line(assigns.stacktrace, assigns.host_otp_app))
+    assigns =
+      assign(
+        assigns,
+        :line,
+        StacktraceFormatter.last_stacktrace_line(assigns.stacktrace, assigns.host_otp_app)
+      )
 
     ~H"""
     <span :if={@line} class="text-xs text-tower-text-secondary line-clamp-1">{@line}</span>
